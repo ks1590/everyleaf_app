1952d5ccb821bbd35fe5689184c30600a255ff0b
kensuke suzuki <35k.0101@gmail.com>
2021-01-09 10:42:22 +0900

    タスク新規登録フォーム作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def new
+    @task = Task.new
+    @task.deadline = Date.today
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを登録しました。"
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def show
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(
+      :title,
+      :dtail,
+      :deadline,
+      :priority,
+      :status
+    ) 
+  end
+  
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true
-  validates :detail presence: true
+  validates :detail, presence: true
   validates :deadline, presence: true
   validates :priority, presence: true
   validates :status, presence: true
