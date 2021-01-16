bb63999386da51f89ad70a4d996c40bb4627296d
kensuke suzuki <35k.0101@gmail.com>
2021-01-16 18:56:16 +0900

    ログイン機能を実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  before_action :authenticate_user, {only: [:edit, :destroy]}
+
+  def new
+    if logged_in?
+      redirect_to tasks_path
+    end
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "users#new"
   resources :tasks
   resources :users
+  resources :sessions, only: [:new, :create, :edit, :destroy]
 end
