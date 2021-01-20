41de04e979ada1bdca44de0b97d553507990dc53
kensuke suzuki <35k.0101@gmail.com>
2021-01-20 23:14:56 +0900

    seed作成

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-
 Bundler.require(*Rails.groups)
 
 module EveryleafApp
@@ -10,6 +9,7 @@ module EveryleafApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
                       model_specs: true,
